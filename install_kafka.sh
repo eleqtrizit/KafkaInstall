c20348bd6fecca166c52882798c7e7fb3ff0d8a8
eleqtrizit <agustin@rivera-web.com>
2019-08-12 18:01:07 +0000

    Syntax cleaning

--- install_kafka.sh
+++ install_kafka.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
 sudo useradd kafka -m
+echo Enter password for user kafka:
 sudo passwd kafka
 sudo adduser kafka sudo
 
 
 echo Updating packages...
-apt update
+sudo apt update
+sudo apt -y upgrade
 
 echo Installing Java JDK...
-apt -y openjdk-11-jdk-headless
+sudo apt -y install openjdk-11-jdk-headless
 
 echo Resolving Kafka download...
+echo https://kafka.apache.org/downloads
 curl https://kafka.apache.org/downloads -o downloads.html
 closer=$(xmllint --html downloads.html --xpath //a/@href 2>/dev/null | sed 's/ href="\([^"]*\)"/\1/g'  | grep closer | grep -v src | head -n 1)
 
+echo curl $closer
 curl $closer -o mirrors.html
 kafka_download=$(xmllint --html mirrors.html --xpath //a/@href 2>/dev/null | sed 's/ href="\([^"]*\)"/\1/g' | grep -E 'https*://(mirror|apache)' | grep kafka | grep tgz | head -n 1)
 
+echo curl $kafka_download
 echo Downloading Kafka...
 curl $kafka_download -o kafka.tgz
 
@@ -25,10 +30,11 @@ mkdir ~/kafka
 mv kafka.tgz ~/kafka
 cd ~/kafka
 tar -xvzf ~/Downloads/kafka.tgz --strip 1
-cd 
+echo delete.topic.enable=true > ~/kafka/config/server.properties
+cd
 sudo mv kafka /home/kafka
 sudo chown -R kafka:kafka /home/kafka/kafka
-echo delete.topic.enable=true > ~/kafka/config/server.properties
+
 
 sudo cp zookeeper.service /etc/systemd/system/
 sudo cp kafka.service /etc/systemd/system/
